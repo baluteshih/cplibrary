bc4375e84e59e48bbb5e2d07d70c310d2c50433a
baluteshih <brian369999@gmail.com>
2026-02-21 02:13:37 +0800

    Setup and add Ordered Set (#1)

---
+++ DataStructure/OrderedSet.hpp
@@ -0,0 +1,78 @@
+#ifndef __BALU_NUMBERSET_HPP__
+#define __BALU_NUMBERSET_HPP__
+
+#include "DataStructure/BIT.hpp"
+
+template<class T>
+class OrderedSet {
+    vector<T> vals;
+    vector<bool> vis;
+    BIT<int> bit;
+    static vector<T> sort_and_unique(vector<T> _vals) {
+        ranges::sort(_vals);
+        _vals.erase(ranges::unique(_vals).begin(), _vals.end());
+        return _vals;
+    }
+    int idx(int x) {
+        auto it = ranges::lower_bound(vals, x);
+        if (it == vals.end() || *it != x) return -1;
+        return it - vals.begin();
+    }
+    int safe_idx(int x) {
+        int res = idx(x);
+        assert(res != -1);
+        return res;
+    }
+public:
+    OrderedSet(vector<T> _vals): vals(sort_and_unique(_vals)), vis(vals.size()), bit(bit_ceil(vals.size())) {}
+    bool insert(T x) {
+        x = safe_idx(x);
+        if (vis[x]) return false;
+        vis[x] = true;
+        bit.modify(x, 1);
+        return true;
+    }
+    bool erase(T x) {
+        x = safe_idx(x);
+        if (!vis[x]) return false;
+        vis[x] = false;
+        bit.modify(x, -1);
+        return true;
+    }
+    bool exists(T x) {
+        x = idx(x);
+        if (x == -1) return false;
+        return vis[x]; 
+    }
+    int size() {
+        return bit.total();
+    }
+    int lt_count(T x) {
+        int idx = ranges::lower_bound(vals, x) - vals.begin() - 1;
+        return bit.prefix(idx);
+    }
+    int leq_count(T x) {
+        int idx = ranges::upper_bound(vals, x) - vals.begin() - 1;
+        return bit.prefix(idx);
+    }
+    int order(T x) {
+        return leq_count(x);
+    }
+    int kth(int k) { // 0-base query, return 0-base index
+        if (size() <= k) return -1;
+        int res = bit.kth(k);
+        return res;
+    }
+    int leq(T x) { // return 0-base index
+        int res = leq_count(x);
+        return res == 0 ? -1 : kth(res - 1);
+    }
+    int geq(T x) { // return 0-base index
+        int res = lt_count(x);
+        return res == size() ? -1 : kth(res);
+    }
+    const T& operator[](size_t index) const {
+        return vals[index];
+    } 
+};
+#endif // __BALU_NUMBERSET_HPP__
---
+++ default_code.hpp
@@ -0,0 +1,84 @@
+#ifndef __BALU_DEFAULT_CODE__
+#define __BALU_DEFAULT_CODE__
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define X first
+#define Y second
+#define SZ(a) ((int)a.size())
+#define ALL(v) v.begin(), v.end()
+template<class A, class B>
+ostream& operator<<(ostream& os, const pair<A, B> &a) {
+    os << "(" << a.first << ", " << a.second << ")";
+    return os;
+}
+template <typename T>
+concept PrintableContainer = requires(T a) {
+    a.begin();
+    a.end();
+} && !std::derived_from<T, std::basic_string<typename T::value_type>>;
+
+template <PrintableContainer T>
+std::ostream& operator<<(std::ostream& os, const T& a) {
+    os << "[ ";
+    bool first = true;
+    for (const auto& item : a) {
+        if (!first) os << ", ";
+        os << item;
+        first = false;
+    }
+    return os << " ]";
+}
+#ifdef bbq
+#include <experimental/iterator>
+#define safe cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
+#define sepline sepline_() 
+#define debug(a...) debug_(#a, a)
+#define orange(a...) orange_(#a, a)
+void debug_(auto s, auto ...a) {
+    cerr << "\e[1;32m(" << s << ") = (";
+    int f = 0;
+    (..., (cerr << (f++ ? ", " : "") << a));
+    cerr << ")\e[0m\n";
+}
+void orange_(auto s, auto L, auto R) {
+    cerr << "\e[1;33m[ " << s << " ] = [ ";
+    using namespace experimental;
+    copy(L, R, make_ostream_joiner(cerr, ", "));
+    cerr << " ]\e[0m\n";
+}
+void sepline_(int length = 50) {
+    cerr << "\e[1;35m";
+    cerr << string(length, '=');
+    cerr << "\e[0m\n";
+}
+#else
+#define safe ((void)0)
+#define sepline safe
+#define debug(...) safe
+#define orange(...) safe
+#endif
+
+void chmax(auto &x, auto val) {
+    x = max(x, val);
+}
+
+void chmin(auto &x, auto val) {
+    x = min(x, val);
+}
+
+vector<int> count_array(const auto &container, int sz = -1) {
+    if (sz == -1) sz = *ranges::max_element(container) + 1;
+    vector<int> res(sz);
+    for (auto x : container) ++res[x];
+    return res;
+}
+
+template<class T>
+void discretization(vector<T> &vals) {
+    ranges::sort(vals);
+    vals.erase(ranges::unique(vals).begin(), vals.end());
+}
+#endif // __BALU_DEFAULT_CODE__
