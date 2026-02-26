0292cb97217c7550488520504013a0fca450ee34
baluteshih <brian369999@gmail.com>
2026-02-26 21:44:45 +0800

    Update NTT and add convolution_any_mod

--- Polynomial/NTT.hpp
+++ Polynomial/NTT.hpp
@@ -2,28 +2,32 @@
 #define __NTT__
 
 #include "Numeric/Modint.hpp"
+#include "Numeric/primitive_root.hpp"
 
 template<typename T>
 requires std::derived_from<T, internal::modint_base>
 class NTT {
-    int max_size;
-    vector<T> w;
-    T root;
-public:
-    NTT(int n, T _root = internal::primitive_root_constexpr(T::mod())) : max_size(1), root(_root) {
-        while (max_size <= n) max_size <<= 1;
-        w.resize(max_size, 1);
-        T dw = root.pow((T::mod() - 1) / max_size);
-        for (int s = max_size / 2; s; s >>= 1, dw *= dw) {
-            w[s] = 1;
-            for (int j = 1; j < s; ++j) 
-                w[s + j] = w[s + j - 1] * dw;
+    inline static int max_size = 1;
+    inline static std::vector<T> w{1, T(1)};
+    inline static const T root = primitive_root(T::mod());
+    static void set_upper_bound(int n) {
+        if (max_size < n) {
+            while (max_size <= n) max_size <<= 1;
+            w.resize(max_size);
+            std::ranges::fill(w, 1);
+            T dw = root.pow((T::mod() - 1) / max_size);
+            for (int s = max_size / 2; s; s >>= 1, dw *= dw) {
+                w[s] = 1;
+                for (int j = 1; j < s; ++j) 
+                    w[s + j] = w[s + j - 1] * dw;
+            }
         }
     }
-    void ntt(vector<T> &a, bool inv = false) { //0 <= a[i] < P
+public:
+    static void ntt(vector<T> &a, bool inv = false) { //0 <= a[i] < P
         int n = a.size();
         assert((n & (n - 1)) == 0);
-        assert(n <= max_size);
+        if ((int)maxsize() < n) set_upper_bound(n);
         for (int i = 0, j = 1; j < n - 1; ++j) {
             for (int k = n >> 1; (i ^= k) < k; k >>= 1);
             if (j < i) swap(a[i], a[j]);
@@ -42,10 +46,10 @@ public:
         reverse(a.begin() + 1, a.begin() + n);
         for (int i = 0; i < n; ++i) a[i] *= iv;
     }
-    size_t maxsize() {
+    static size_t maxsize() {
         return max_size;
     }
-    vector<T> convolution(vector<T> a, vector<T> b) {
+    static vector<T> convolution(vector<T> a, vector<T> b) {
         if (a.empty() || b.empty()) return vector<T>();
         int n = 1, sz = int(a.size()) + int(b.size()) - 1;
         while (n < sz) n <<= 1;
---
+++ Polynomial/convolution_any_mod.hpp
@@ -0,0 +1,36 @@
+#ifndef __CONVOLUTION_ANY_MOD__
+#define __CONVOLUTION_ANY_MOD__
+
+// source: https://maspypy.github.io/library/poly/convolution.hpp
+
+#include "Numeric/Modint.hpp"
+#include "Polynomial/NTT.hpp"
+#include "Numeric/crt.hpp"
+
+template <typename T>
+requires std::derived_from<T, internal::modint_base>
+vector<T> convolution_any_mod(vector<T> a, vector<T> b) {
+    if (a.empty() || b.empty()) return vector<T>();
+    int n = a.size(), m = b.size();
+    static constexpr int p0 = 167772161;
+    static constexpr int p1 = 469762049;
+    static constexpr int p2 = 754974721;
+    using mint0 = static_modint<p0>;
+    using mint1 = static_modint<p1>;
+    using mint2 = static_modint<p2>;
+    vector<mint0> a0(n), b0(m);
+    vector<mint1> a1(n), b1(m);
+    vector<mint2> a2(n), b2(m);
+    for (int i = 0; i < n; ++i)
+        a0[i] = a[i].val(), a1[i] = a[i].val(), a2[i] = a[i].val();
+    for (int i = 0; i < m; ++i)
+        b0[i] = b[i].val(), b1[i] = b[i].val(), b2[i] = b[i].val();
+    auto c0 = NTT<mint0>::convolution(a0, b0);
+    auto c1 = NTT<mint1>::convolution(a1, b1);
+    auto c2 = NTT<mint2>::convolution(a2, b2);
+    vector<T> c(c0.size());
+    for (int i = 0; i < n + m - 1; ++i)
+        c[i] = CRT3<T, p0, p1, p2>(c0[i].val(), c1[i].val(), c2[i].val());
+    return c;
+}
+#endif // __CONVOLUTION_ANY_MOD__
