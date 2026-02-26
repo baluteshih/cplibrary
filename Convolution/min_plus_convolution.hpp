713305ea6e5bb9daa1d776a9b65dc3b8e35bf8bf
baluteshih <brian369999@gmail.com>
2026-02-26 22:18:10 +0800

    Add min_plus_convolution

---
+++ Convolution/min_plus_convolution.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+// a is convex, i.e. a[i + 1] - a[i] <= a[i + 2] - a[i + 1]
+template<typename T>
+vector<T> min_plus_convolution(vector<T> &a, vector<T> &b) {
+    int n = a.size(), m = b.size();
+    vector<T> c(n + m - 1, std::numeric_limits<T>::max());
+    auto dc = [&](auto Y, int l, int r, int jl, int jr) {
+        if (l > r) return;
+        int mid = (l + r) / 2, from = -1;
+        T &best = c[mid];
+        for (int j = jl; j <= jr; ++j)
+            if (int i = mid - j; i >= 0 && i < n)
+                if (best > a[i] + b[j]) 
+                    best = a[i] + b[j], from = j;
+        Y(Y, l, mid - 1, jl, from), Y(Y, mid + 1, r, from, jr);
+    };
+    return dc(dc, 0, n - 1 + m - 1, 0, m - 1), c;
+}
