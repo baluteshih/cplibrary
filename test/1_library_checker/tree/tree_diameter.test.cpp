c8ffe94c80a569f938385e049e4f9554684835a7
baluteshih <brian369999@gmail.com>
2026-03-21 16:00:14 +0800

    Add Tree

---
+++ test/1_library_checker/tree/tree_diameter.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/tree_diameter"
+#include "default_code.hpp"
+
+#include "Tree/Tree.cpp"
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int n;
+    cin >> n;
+    Tree<ll> tree(n);
+    for (int i = 1; i < n; ++i) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        tree.add_edge(u, v, w);
+    }
+    auto dis = tree.distance_edge(0);
+    int s = ranges::max_element(dis) - dis.begin();
+    dis = tree.distance_edge(s);
+    int t = ranges::max_element(dis) - dis.begin();
+    auto res = tree.path(s, t);
+    cout << dis[t] << " " << res.size() << "\n";
+    for (int i = 0; i < SZ(res); ++i)
+        cout << res[i] << " \n"[i + 1 == SZ(res)];
+}
