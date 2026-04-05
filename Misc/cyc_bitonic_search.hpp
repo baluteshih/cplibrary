baa2b85a443d6d7106b028d340ab444761286f6a
baluteshih <brian369999@gmail.com>
2026-04-06 04:33:28 +0800

    add furthest_pair

--- Misc/cyc_bitonic_search.hpp
+++ Misc/cyc_bitonic_search.hpp
@@ -2,6 +2,7 @@
 
 /* 
    bool pred(int a, int b);
+   a is better than b
    f(0) ~ f(n - 1) is a cyclic-shift bitonic-function
    return idx s.t. pred(x, idx) is false forall x
 */
@@ -14,4 +15,26 @@ int cyc_bitonic_search(int n, auto pred) {
         else l = m;
     }
     return pred(l, r % n) ? l : r % n;
+}
+
+/* 
+   bool pred(int o, int a, int b);
+   a is better than b
+   f(o, 0) ~ f(o, n - 1) is a cyclic-shift bitonic-function
+   return idx[o] s.t. pred(o, x, idx[o]) is false forall x
+   idx should be non-decreasing, and idx[o] != o
+*/
+std::vector<int> rotating_calipers(int n, auto pred) {
+    assert(n >= 2);
+    std::vector<int> res(n, 1);
+    for (int i = 2; i < n; ++i)
+        if (pred(0, i, res[0]))
+            res[0] = i;
+    for (int i = 1; i < n; ++i) {
+        res[i] = res[i - 1];
+        while ((res[i] + 1) % n != i && pred(i, (res[i] + 1) % n, res[i] % n))
+            ++res[i];
+        res[i] %= n;
+    }
+    return res;
 }
