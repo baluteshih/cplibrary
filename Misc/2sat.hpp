4884dc0b46f8d9ef43ee13056a94539bcba2163e
baluteshih <brian369999@gmail.com>
2026-04-16 17:33:30 +0800

    add 2sat

---
+++ Misc/2sat.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Graph/SCC.hpp"
+
+struct SAT { // 0-base
+    int n;
+    std::vector<bool> istrue;
+    SCC<> scc;
+    SAT(int _n): n(_n), istrue(n + n), scc(n + n) {}
+    int rv(int a) {
+        return a >= n ? a - n : a + n;
+    }
+    void add_clause(int a, int b) {
+        scc.add_edge(rv(a), b), scc.add_edge(rv(b), a);
+    }
+    bool solve() {
+        scc.solve();
+        for (int i = 0; i < n; ++i) {
+            if (scc.bln[i] == scc.bln[i + n]) return false;
+            istrue[i] = scc.bln[i] < scc.bln[i + n];
+            istrue[i + n] = !istrue[i];
+        }
+        return true;
+    }
+};
