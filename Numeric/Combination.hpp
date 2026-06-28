f2119e27bef92b6e9e29ac01a6b2332ab2a03237
baluteshih <brian369999@gmail.com>
2026-06-29 01:08:03 +0800

    update Combination

--- Numeric/Combination.hpp
+++ Numeric/Combination.hpp
@@ -5,25 +5,18 @@
 template<class T>
 requires std::derived_from<T, internal::modint_base>
 class Combination {
-    int N;
-    void init() {
-        for (int i = 1; i <= N; ++i)
-            fac[i] = fac[i - 1] * i;
-        ifac.back() = fac.back().inv();
-        for (int i = N - 1; i >= 0; --i)
-            ifac[i] = ifac[i + 1] * (i + 1);
-    }
+    inline static int N = 1;
 public:
-    std::vector<T> fac, ifac;
-    Combination(int n): N(n), fac(N + 1, 1), ifac(N + 1, 1) {
-        init();
-    }
-    Combination(int n, T base): N(n), fac(N + 1, base.raw(1)), ifac(N + 1, base.raw(1)) {
-        init();
-    }
+    inline static std::vector<T> fac = {T(1)};
+    inline static std::vector<T> ifac = {T(1)};
+    Combination(int n) { ensure_upper_bound(n); }
     T C(int n, int m) {
-        if (n < m) return 0;
+        if (n < m || m < 0) return 0;
         return fac[n] * ifac[m] * ifac[n - m];
+    }
+    T invC(int n, int m) {
+        assert(n >= m && m >= 0);
+        return ifac[n] * fac[m] * fac[n - m];
     }
     T P(int n, int m) {
         if (n < m) return 0;
@@ -31,6 +24,21 @@ public:
     }
     T H(int n, int m) {
         return C(n + m - 1, m);
+    }
+    // a - b <= k and all non-empty proper prefix have a - b < k
+    T extend_catalan(int a, int b, int k) {
+        if (a - b == k) return C(a + b - 1, a - 1) - C(a + b - 1, b + k);
+        return C(a + b, a) - C(a + b, b + k); 
+    }
+    void ensure_upper_bound(int n) {
+        if (N >= n) return;
+        fac.resize(n), ifac.resize(n);
+        for (int i = N; i < n; ++i)
+            fac[i] = fac[i - 1] * i;
+        ifac.back() = fac.back().inv();
+        for (int i = n - 2; i >= N; --i)
+            ifac[i] = ifac[i + 1] * (i + 1);
+        N = n;
     }
 };
 namespace CombFunc {
