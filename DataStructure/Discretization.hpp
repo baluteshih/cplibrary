5981866d0682a8b5e250695a9e77d497b22f131d
baluteshih <brian369999@gmail.com>
2026-06-30 17:54:41 +0800

    update Discretization

--- DataStructure/Discretization.hpp
+++ DataStructure/Discretization.hpp
@@ -3,10 +3,13 @@
 template<typename T>
 class Discretization {
     std::vector<T> vals;
-    static std::vector<T> sort_and_unique(std::vector<T> _vals) {
-        std::ranges::sort(_vals);
-        _vals.erase(std::ranges::unique(_vals).begin(), _vals.end());
-        return _vals;
+    static std::vector<T> sort_and_unique(std::ranges::range auto &&_vals) {
+        std::vector<T> res;
+        std::ranges::copy(_vals, std::back_inserter(res));
+        std::ranges::sort(res);
+        auto [first, last] = std::ranges::unique(res);
+        res.erase(first, last);
+        return res;
     }
 public:
     int idx(T x) {
@@ -19,7 +22,7 @@ public:
         assert(res != -1);
         return res;
     }
-    Discretization(const std::ranges::range auto &_vals): vals(sort_and_unique(_vals)) {}
+    Discretization(std::ranges::range auto &&_vals) : vals(sort_and_unique(std::forward<decltype(_vals)>(_vals))) {}
     int left_close(T x) {
         return std::ranges::lower_bound(vals, x) - vals.begin();
     }
@@ -39,3 +42,6 @@ public:
         return vals.size();
     }
 };
+
+template <std::ranges::range R>
+Discretization(R&&) -> Discretization<std::ranges::range_value_t<R>>;
